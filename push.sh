7625f1b7563aac2112a61f0598778aa2fe4491d1
kaiwen0425 <kaiweno425@gmail.com>
2019-07-28 10:00:10 +0800

    test

---
+++ push.sh
@@ -0,0 +1,18 @@
+commit_info=$1
+if [ -z "$1" ]
+then
+   echo "place input commit_info"   
+   read commit_info
+fi
+echo "bake theme_setting"
+cp -f themes/hexo-theme-next/_config.yml theme_setting_bak
+
+echo ""
+hexo clean
+hexo d -g
+
+echo "back all file to github"
+git state
+git add .
+git commit -m ${commit_info}
+git push origin master
