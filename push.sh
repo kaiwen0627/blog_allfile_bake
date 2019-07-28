d95f116b095d344139e5522c34acbe53f906aa47
kaiwen0425 <kaiweno425@gmail.com>
2019-07-28 11:46:27 +0800

    test

--- push.sh
+++ push.sh
@@ -6,7 +6,7 @@ then
 fi
 echo "bake theme_setting"
 cp -f themes/hexo-theme-next/_config.yml theme_setting_bak
-
+cp -f themes/hexo-theme-next/source/css/_custom/custom.styl theme_setting_bak
 echo ""
 hexo clean
 hexo d -g
@@ -14,5 +14,6 @@ hexo d -g
 echo "back all file to github"
 git status
 git add .
+git status
 git commit -m ${commit_info}
 git push origin master
